0dc5efc6ea26d17ca71bb5164f78a43ff7dcce4a
rudananastasija <rudananastasija@uns.ac.rs>
2018-12-01 15:44:38 +0100

    dorada glavne strane

--- isa/src/main/resources/static/js/mainPage.js
+++ isa/src/main/resources/static/js/mainPage.js
@@ -1,3 +1,28 @@
 /**
  * 
  */
+
+function onLoad(){
+	
+	$("#pozadinaAuto").hide();
+	$("#pozadinaHotel").hide();
+}
+
+function planeShow(){
+	$("#pozadinaAvion").show();
+	$("#pozadinaAuto").hide();
+	$("#pozadinaHotel").hide();
+
+	
+}
+function hotelShow(){
+	$("#pozadinaAvion").hide();
+	$("#pozadinaAuto").hide();
+	$("#pozadinaHotel").show();
+}
+function carShow(){
+	$("#pozadinaAvion").hide();
+	$("#pozadinaAuto").show();
+	$("#pozadinaHotel").hide();
+	
+}
