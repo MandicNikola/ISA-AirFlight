955d2535183e9b8c47bdc301da57300d257d9212
rudananastasija <rudananastasija@uns.ac.rs>
2018-12-04 18:15:28 +0100

    izmjena main

--- isa/src/main/resources/static/js/mainPage.js
+++ isa/src/main/resources/static/js/mainPage.js
@@ -19,10 +19,44 @@ function hotelShow(){
 	$("#pozadinaAvion").hide();
 	$("#pozadinaAuto").hide();
 	$("#pozadinaHotel").show();
+	
+	$.ajax({
+		method:'GET',
+		url: "../restorani/vratiDostava",
+		success: function(lista){
+			if(lista == null){
+				nemaPor();
+				
+			}else{
+				ispisiDostavu(lista);
+				
+				
+			}
+		}
+	});
 }
 function carShow(){
 	$("#pozadinaAvion").hide();
 	$("#pozadinaAuto").show();
 	$("#pozadinaHotel").hide();
+	
+	
+	
+	$.ajax({
+		method:'GET',
+		url: "/api/rents/",
+		success: function(lista){
+			if(lista == null){
+				console.log('Nema auta')
+			}else{
+				ispisiAuta(lista);
+				
+				
+			}
+		}
+	});
+}
+function ispisiAuta(lista){
+	
 	
 }
