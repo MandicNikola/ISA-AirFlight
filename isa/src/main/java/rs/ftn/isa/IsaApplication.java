38c83b72ce95b97e44c74561d7047bafe53f428d
rudananastasija <rudananastasija@uns.ac.rs>
2018-12-09 21:03:44 +0100

    izmjena modela

--- isa/src/main/java/rs/ftn/isa/IsaApplication.java
+++ isa/src/main/java/rs/ftn/isa/IsaApplication.java
@@ -1,12 +1,38 @@
 package rs.ftn.isa;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import rs.ftn.isa.model.PricelistHotel;
+import rs.ftn.isa.model.PricelistRentCar;
+import rs.ftn.isa.model.Usluga;
 
 @SpringBootApplication
 public class IsaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IsaApplication.class, args);
+		Usluga usluga1 = new Usluga("WIFI",1200);
+		Usluga usluga2 = new Usluga("Bazen", 6000);
+
+		PricelistHotel cenovnik = new PricelistHotel();
+
+		@SuppressWarnings("deprecation")
+		Date datum = new Date(2017, 5, 5);
+
+		cenovnik.setDatum_primene(datum);
+
+		usluga1.setCijene(cenovnik);
+		usluga2.setCijene(cenovnik);
+
+		Set<Usluga> usluge = new HashSet<Usluga>();
+		usluge.add(usluga1);
+		usluge.add(usluga2);
+
+		cenovnik.setUsluge(usluge);
 	}
 }
