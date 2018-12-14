b9010ef85bce1b4896aac0b5d9b937224667116e
marinagavric <marina.gavric@uns.ac.rs>
2018-12-14 13:24:40 +0100

    Testiranje

--- isa/src/main/java/rs/ftn/isa/IsaApplication.java
+++ isa/src/main/java/rs/ftn/isa/IsaApplication.java
@@ -74,9 +74,7 @@ public class IsaApplication {
 		usluge.add(usluga4);
 		cenovnik.setUsluge(usluge);
 		
-<<<<<<< HEAD
-	}
-=======
+	
 		Set<Usluga> usluge2 = new HashSet<Usluga>();
 		usluge.add(usluga3);
 		
@@ -118,9 +116,6 @@ public class IsaApplication {
 		servis4.saveRentACar(serviss);
 		
 		
-	
+	}
 		
-		}
-
->>>>>>> 06478911ef91728e5c821c097533739e4f72e748
 }
