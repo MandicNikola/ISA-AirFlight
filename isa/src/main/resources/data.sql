dfd45c66b5816771752c72979273b43fc6ac804d
marinagavric <marina.gavric@uns.ac.rs>
2018-12-20 17:03:46 +0100

    Izmene

--- isa/src/main/resources/data.sql
+++ isa/src/main/resources/data.sql
@@ -1,9 +1,9 @@
 insert into user ( id, ime, prezime, mail, telefon, grad, verifikovan, lozinka) values (4564,'Marko', 'Marković','marko@gmail.com', 063215258, 'Novi Sad', 'da', 'mare');
 insert into user ( id, ime, prezime, mail, telefon, grad, verifikovan, lozinka) values (42164,'Marina', 'Marić','marina@gmail.com', 0645691,'Beograd', 'ne','mare');
-insert into hotel ( id, naziv, adresa, opis, ocena) values (1245,'Marinas', 'Ljubinje','divno', 10);
-insert into hotel ( id, naziv, adresa, opis, ocena) values (1115,'Anjica', 'Novi Sad','divno', 3);
+insert into hotel ( id, naziv, adresa, opis, ocena) values (1245,'Marinas', 'Temerinska 10','divno', 10);
+insert into hotel ( id, naziv, adresa, opis, ocena) values (1115,'Anjica', 'Tekelijina 18','divno', 3);
 insert into cenovnik( id, datum_primene,aktivan) values (125,'2018-11-11',false);
 insert into cenovnik( id, datum_primene,aktivan) values (128,'2017-11-11',false);
-insert into rentacar ( id, naziv, adresa, opis) values (1245,'Caaar', 'Novi Sad','samo brzo');
-insert into rentacar ( id, naziv, adresa, opis) values (1255,'Rent a caar', 'Novi Sad','sigurno i lako');
-insert into rentacar ( id, naziv, adresa, opis) values (1265,'Caaar the best', 'Novi Sad','samo brzo');
+insert into rentacar ( id, naziv, adresa, opis) values (1245,'Caaar', 'Temerinska 10','samo brzo');
+insert into rentacar ( id, naziv, adresa, opis) values (1255,'Rent a caar', 'Tekelijina 18','sigurno i lako');
+insert into rentacar ( id, naziv, adresa, opis) values (1265,'Caaar the best', 'Tekelijina 22','samo brzo');
