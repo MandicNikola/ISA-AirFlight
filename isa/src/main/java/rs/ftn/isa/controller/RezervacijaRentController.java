101793cc1033b16989b59c709c2f286a0463d800
marinagavric <marina.gavric@uns.ac.rs>
2019-01-25 13:19:37 +0100

    Zapoceto ocenjivanje

--- isa/src/main/java/rs/ftn/isa/controller/RezervacijaRentController.java
+++ isa/src/main/java/rs/ftn/isa/controller/RezervacijaRentController.java
@@ -1,14 +1,21 @@
 package rs.ftn.isa.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Context;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import rs.ftn.isa.model.RezervacijaRentCar;
+import rs.ftn.isa.model.User;
 import rs.ftn.isa.service.RezervacijaRentServiceImpl;
 
 @RestController
@@ -23,5 +30,36 @@ public class RezervacijaRentController {
 		return  servis.findAll();
 	}
 	
-	
+	@RequestMapping(value="/istorijaRent",
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ArrayList<RezervacijaRentCar> getHistoryRent(@Context HttpServletRequest request){		
+		ArrayList<RezervacijaRentCar> rezervacije = new ArrayList<RezervacijaRentCar>();
+		User korisnik = (User)request.getSession().getAttribute("ulogovan");		
+		System.out.println("Usao u getHis rent");
+		if(korisnik!=null) {
+			System.out.println("Neko je ulogovan");
+			Long idKor=korisnik.getId();
+			String idKorS=idKor.toString();
+			System.out.println("Id je "+idKor);
+			
+			List<RezervacijaRentCar> sveRez=servis.findAll();
+			System.out.println("Ukupan broj rez "+sveRez.size());
+			
+			for(RezervacijaRentCar rezervacija:sveRez) {
+				Long idRezKor=rezervacija.getKorisnik().getId();
+				String idRezS=idRezKor.toString();
+				System.out.println("Id rez korisnik je "+idRezKor);
+				if(idKorS.equals(idRezS)) {
+					System.out.println("Jednaki su");
+						System.out.println("Ima rezervacija");
+						System.out.println("Dodata rezervacija sa check in"+rezervacija.getDatumPreuzimanja());
+						System.out.println("Id je "+rezervacija.getId());
+						rezervacije.add(rezervacija);
+					}
+				}
+		}
+		return rezervacije;
+	}
+
 }
