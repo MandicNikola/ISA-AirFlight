46f9b0f8ec9b35c80a393ea0f1b0cfcb1cf823b1
marinagavric <marina.gavric@uns.ac.rs>
2019-01-31 11:52:27 +0100

    Zapoceti grafici

--- isa/src/main/java/rs/ftn/isa/controller/RezervacijaRentController.java
+++ isa/src/main/java/rs/ftn/isa/controller/RezervacijaRentController.java
@@ -1,6 +1,7 @@
 package rs.ftn.isa.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,13 +10,16 @@ import javax.ws.rs.core.Context;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import rs.ftn.isa.dto.ChartDTO;
 import rs.ftn.isa.model.RezervacijaRentCar;
 import rs.ftn.isa.model.User;
+import rs.ftn.isa.model.Vehicle;
 import rs.ftn.isa.service.RezervacijaRentServiceImpl;
 
 @RestController
@@ -61,5 +65,43 @@ public class RezervacijaRentController {
 		}
 		return rezervacije;
 	}
+
+	@RequestMapping(value="/dailychart/{idRent}",
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ArrayList<ChartDTO> getDailyChart(@PathVariable String idRent){		
+			System.out.println("Usao u getDaily chart");
+			ArrayList<ChartDTO> podaci = new ArrayList<ChartDTO>();
+			List<RezervacijaRentCar> sveRez=servis.findAll();
+	
+			//treba da nadjemo sve rezervacije od hotela sa idRez
+			for(RezervacijaRentCar rezervacija:sveRez) {
+				Vehicle vozilo = rezervacija.getVozilo();
+				String idServis = vozilo.getFilijala().getServis().getId().toString();
+				
+				if(idServis.equals(idRent)) {
+					//dodajemo u listu
+					Date datum  = rezervacija.getDatumPreuzimanja();
+					ChartDTO noviPodatak = null;
+					for(ChartDTO chart: podaci) {
+						if(chart.getDatum().equals(datum)) {
+							noviPodatak=chart;
+							break;
+						}
+					}
+					if(noviPodatak != null) {
+						podaci.remove(noviPodatak);
+						int dosadasnjiBroj = noviPodatak.getBroj();
+						noviPodatak.setBroj(dosadasnjiBroj+1);
+						podaci.add(noviPodatak);
+					}else {
+						noviPodatak = new ChartDTO(datum, 1);
+						podaci.add(noviPodatak);
+					}
+				}
+			}
+			return podaci;
+	}
+
 
 }
