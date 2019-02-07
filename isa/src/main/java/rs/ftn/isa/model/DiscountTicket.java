2888c65d362102312a392a8c6928abb2aa32c48e
MandicNikola <mandic_nikola96@uns.ac.rs>
2019-02-07 20:05:44 +0100

    izmenjeno

---
+++ isa/src/main/java/rs/ftn/isa/model/DiscountTicket.java
@@ -0,0 +1,86 @@
+package rs.ftn.isa.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class DiscountTicket {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "vrijednost", nullable = false)	
+	private int vrednost;
+	
+	@Column(name = "bodovi", nullable = false)	
+	private int bodovi;
+	
+	
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Ticket kartaPopust;
+	
+	
+	
+	public DiscountTicket()
+	{
+		
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public int getVrednost() {
+		return vrednost;
+	}
+
+
+
+	public void setVrednost(int vrednost) {
+		this.vrednost = vrednost;
+	}
+
+
+
+	public int getBodovi() {
+		return bodovi;
+	}
+
+
+
+	public void setBodovi(int bodovi) {
+		this.bodovi = bodovi;
+	}
+
+
+
+	public Ticket getKartaPopust() {
+		return kartaPopust;
+	}
+
+
+
+	public void setKartaPopust(Ticket kartaPopust) {
+		this.kartaPopust = kartaPopust;
+	}
+	
+	
+	
+}
