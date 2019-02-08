91d0a7f7b7ad639431a091785fe100435b81f0a1
marinagavric <marina.gavric@uns.ac.rs>
2019-02-08 19:12:51 +0100

    Izmene

---
+++ isa/src/test/java/rs/ftn/isa/constants/FilijalaConstants.java
@@ -0,0 +1,10 @@
+package rs.ftn.isa.constants;
+
+
+public class FilijalaConstants {
+	public static final Long DB_ID =(long) 3313;
+    public static final String DB_GRAD = "Beograd";
+    public static final String DB_ULICA = "Bulevar";
+    public static final String DB_SERVIS = "1245";
+
+}
